dba2e30fa4e7708b8b0d38bb8f51fe4fb77c9d8a
C Selvaraju <cselvaraju@gmail.com>
2022-02-25 17:54:55 +0530

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <ComputedPropDemo />
+</template>
+
+<script>
+import ComputedPropDemo from './components/ComptedPropDemo.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ComputedPropDemo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ComptedPropDemo.vue
@@ -0,0 +1,48 @@
+<template>
+  <h1>Computed Props Demo</h1>
+  Full Name: {{ firstName }} {{ lastName }}
+  <br />
+  Full Name: {{ firstName + " " + lastName }}
+  <br />
+  Full Name using Method: {{ fullNameMethod() }}
+  <br />
+  Full Name using Computed Prop: {{ fullNameComputedProp }}
+  <p>
+      <h3>Actor Details</h3>
+      <p>{{ actor.name }}</p>
+      <p>Movie Info: {{ movieInfo }} </p>
+  </p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      firstName: "Amitabh",
+      lastName: "Bachhan",
+      actor: {
+          name: 'Shashi Kapoor',
+          movies: [
+              'Deewar', 'Muqadda Ka Sikandar'
+          ]
+      }
+    };
+  },
+  methods: {
+    fullNameMethod() {
+      return this.firstName + " " + this.lastName;
+    },
+  },
+  computed: {
+    fullNameComputedProp() {
+      return this.firstName + " " + this.lastName;
+    },
+    movieInfo() {
+        return this.actor.movies.length !== 0 ? this.actor.movies.length + ' movies' : 'No movies info!!'
+    }
+  },
+};
+</script>
+
+<style>
+</style>
